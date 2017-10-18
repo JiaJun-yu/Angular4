3d587a461698ca2fe3ffb3400ddaa73a1d614ffb
shakalakay <729157930@qq.com>
2017-10-18 22:02:31 +0800

    childrenRoute

--- router/src/app/app-routing.module.ts
+++ router/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {ProductComponent} from './product/product.component';
 import {Code404Component} from './code404/code404.component';
+import { ProductDescComponent } from "./product-desc/product-desc.component";
+import { SellerInfoComponent } from "./seller-info/seller-info.component";
 
 const routes: Routes = [
   //重定向路由，空字符串的路由指向'/home'
   {path:'', redirectTo: '/home', pathMatch: 'full'},
   {path:'home', component: HomeComponent},
-  {path:'product/:id', component: ProductComponent},
+  {path:'product/:id', component: ProductComponent,
+    children:[
+      {path:'',component: ProductDescComponent},
+      {path:'seller/:id',component: SellerInfoComponent},
+    ]
+  },
   {path:'**',component: Code404Component},
 ];
 
